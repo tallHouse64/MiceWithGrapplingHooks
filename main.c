40a5a66e756ac27c823adfa3eee05c278b3017de
tallHouse64 <tallhouse64@protonmail.com>
2025-12-21 12:30:15 +0000

    Added boilerplate.

---
+++ main.c
@@ -0,0 +1,40 @@
+#define D_IMPLEMENTATION
+#include"d.h"
+
+#define DEVENTS_IMPLEMENTATION
+#include"devents.h"
+
+#define D_PLATFORM_IMPLEMENTATION
+#include"platform/sdld.h"
+
+#define DELAY 1000/30
+
+int main(int argc, char ** argv){
+    D_Surf * out = D_GetOutSurf(50, 50, 640, 480, "Mice With Grappling Hooks", 0);
+    int running = 1;
+    D_Event e = {0};
+    D_StartEvents();
+
+    while(running){
+        D_PumpEvents();
+
+        while(D_GetEvent(&e) != -1){
+            switch(e.type){
+                case D_QUIT:
+                    running = 0;
+                    break;
+            };
+        };
+
+        D_FlipOutSurf(out);
+
+        D_Delay(DELAY);
+    };
+
+    D_StopEvents();
+
+    D_FreeOutSurf(out);
+    out = D_NULL;
+
+    return 0;
+};
