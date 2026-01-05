3b0cabc0a6918ae334a62a25a9a3e4c1a12b8c39
tallHouse64 <tallhouse64@protonmail.com>
2026-01-05 17:01:05 +0000

    Added include guards to physics.h.

--- physics/physics.h
+++ physics/physics.h
@@ -1,5 +1,8 @@
-
+#ifndef MWG_PHYSICS_H
+#define MWG_PHYSICS_H
 
 int MWG_FindEntryPoint(MWG_Player * player, MWG_MapRect * rect, int * xEntryPoint, int * yEntryPoint);
 
 int MWG_CalcPhysics(MWG_Map * map);
+
+#endif
