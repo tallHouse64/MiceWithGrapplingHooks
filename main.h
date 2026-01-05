6bf44b5551e7edaf4f3883941a4acc6c4ca8dc29
tallHouse64 <tallhouse64@protonmail.com>
2026-01-05 16:39:54 +0000

    Added main.h, also moved MWG_Player, MWG_MapRect, MWG_Map structs to it.

---
+++ main.h
@@ -0,0 +1,25 @@
+
+typedef struct MWG_Player {
+
+    /* This x, y position is the bottom-middle of
+     *  the player (like maro 64). */
+    int x, y;
+
+    int oldX, oldY;
+
+    D_double angle;
+} MWG_Player;
+
+typedef struct MWG_MapRect {
+    int x, y, w, h;
+
+    /* Box colour */
+    int r, g, b;
+} MWG_MapRect;
+
+typedef struct MWG_Map {
+    MWG_MapRect rect[MWG_MAX_MAP_RECTS];
+    int numRects;
+    MWG_Player player[MWG_MAX_PLAYER];
+    int numPlayers;
+} MWG_Map;
