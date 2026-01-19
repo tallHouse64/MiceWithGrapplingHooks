2541c044a1f8a0af43d857e8b4cca90edc77dc52
tallHouse64 <tallhouse64@protonmail.com>
2026-01-19 19:08:06 +0000

    MWG_CalcPhysics() now applies grappling hook forces.

--- physics/physics.c
+++ physics/physics.c
@@ -479,6 +479,33 @@ int MWG_CalcPhysics(MWG_Map * map){
         map->player[i].oldX = LERP_INT(map->player[i].oldX, map->player[i].x, 64);
         map->player[i].oldY = LERP_INT(map->player[i].oldY, map->player[i].y, 64);
 
+        if(map->player[i].hookState == MWG_HOOK_ATTACHED){
+
+            /* Is the player to the right of the
+             *  grappling hook? */
+            if(map->player[i].x > map->player[i].hookX){
+                /* Apply a force, moving the
+                 *  player left. */
+                map->player[i].oldX = map->player[i].oldX + 10;
+            }else{
+                /* Apply a force, moving the
+                 *  player right. */
+                map->player[i].oldX = map->player[i].oldX - 10;
+            };
+
+            /* Is the player to the above the
+             *  grappling hook? */
+            if(map->player[i].y < map->player[i].hookY){
+                /* Apply a force, moving the
+                 *  player down. */
+                map->player[i].oldY = map->player[i].oldY - 10;
+            }else{
+                /* Apply a force, moving the
+                 *  player up. */
+                map->player[i].oldY = map->player[i].oldY + 10;
+            };
+        };
+
 
         /* Stop the player moving when the speed
          *  is very slow (this is probably
