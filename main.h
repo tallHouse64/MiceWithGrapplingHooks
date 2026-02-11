609833ecf6095938f7abf898a3fc328c96ffe244
tallHouse64 <tallhouse64@protonmail.com>
2026-02-11 12:36:05 +0000

    Added MWG_PLAYER_FLYING flag to MWG_PlayerFlags.

--- main.h
+++ main.h
@@ -60,7 +60,8 @@ typedef enum MWG_CollisionDirection {
 typedef enum MWG_PlayerFlags {
     MWG_PLAYER_NO_FLAG      = 0,
     MWG_PLAYER_CAN_EDIT     = 0x1,
-    /* add flying and hook enabled flags? */
+    MWG_PLAYER_FLYING       = 0x2
+    /* Add a hook enabled flag? */
 } MWG_PlayerFlags;
 
 typedef enum MWG_HookState {
